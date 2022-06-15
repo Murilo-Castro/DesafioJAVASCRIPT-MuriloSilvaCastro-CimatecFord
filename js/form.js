c7fef2c9a27a9b41f141bfd9830f49887aaa659d
Murilo_Castro <murilo.65@gmail.com>
2022-06-15 08:18:21 -0300

    Atrlando o botão enviar ao checkbox do termos de privacidade

--- js/form.js
+++ js/form.js
@@ -1,8 +1,22 @@
+let check = document.querySelector("#consentimento");
+let btn = document.querySelector("button");
+btn.disabled = true;
+
+check.addEventListener("input", function(e){
+    if(check.checked){
+        btn.disabled = false;
+        btn.style.cursor = "pointer";
+    } else{
+        btn.disabled = true;
+        btn.style.cursor = "auto";
+    }
+})
+
 criaId();
 
 //class contato
 class contato {
-    constructor(nome, sobrenome, email, cpf, telefone, contato, mensage) {
+    constructor(nome, sobrenome, email, cpf, telefone, contato, mensage, consentimento, promos) {
         this._nome = nome;
         this._sobrenome = sobrenome;
         this._email = email;
@@ -10,6 +24,8 @@ class contato {
         this._telefone = telefone;
         this._contato = contato;
         this._mensage = mensage;
+        this._consentimento = consentimento;
+        this._promos = promos;
     }
 
     get nome() {
@@ -24,14 +40,15 @@ function Post(form) {
         form.elements.namedItem("cpf").value,
         form.elements.namedItem("telefone").value,
         form.elements.namedItem("contato").value,
-        form.elements.namedItem("mensage").value);
+        form.elements.namedItem("mensage").value,
+        form.elements.namedItem("consentimento").value,
+        form.elements.namedItem("aceite-promo").value);
     Enviar(data);
 }
 
 function Enviar(contact) { 
     const contato =contact
     console.log(contato);
-    console.log(contato._mensage);
 
     var nome = document.getElementById("nomeid");
     if (nome.value != "") {
